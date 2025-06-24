d5e99eeec8e93ecc89a3f2e5a260903cfc2ec589
Bret Mda <146015182+br3tdev@users.noreply.github.com>
2025-06-24 12:12:18 +0300

    Create Landing page (#49)
    Co-authored-by: bret <bretai@LCARS-MX-23>

---
+++ components/app/nav-bar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        TripSync
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <a class="btn btn-accent">Sign In with
+        <Icon name="tabler:brand-github" size="24" />
+      </a>
+    </div>
+  </div>
+</template>
