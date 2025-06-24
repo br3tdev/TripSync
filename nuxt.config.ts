a9c2da8a89e606c994002a68133d7002f35997c1
bret <bretai@LCARS-MX-23>
2025-06-24 13:00:40 +0300

    :sparkles: Add theme toogle

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode"],
   css: ["~/assets/css/main.css"],
   eslint: {
     config: {
@@ -14,5 +15,8 @@ export default defineNuxtConfig({
     plugins: [
       tailwindcss(),
     ],
+  },
+  colorMode: {
+    dataValue: "theme",
   },
 });
