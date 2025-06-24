398a3a537ea25460e81992cb32adee85086b3ef2
bret <bretai@LCARS-MX-23>
2025-06-24 12:08:00 +0300

    Create Landing page

--- app.vue
+++ app.vue
@@ -1,13 +1,5 @@
 <template>
-  <div>
-    <!-- <NuxtRouteAnnouncer />
-    <NuxtWelcome /> -->
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-secondary">
-      <Icon name="uil:github" style="color: black; margin-right: 8px;" />
-      Secondary
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hero bg-base-300 container mx-auto mt-4">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Logs
+        </h1>
+        <p class="py-6">
+          TripSync effortlessly organizes your trips, memories, and routes—so you can focus on the experience, not the planning.
+        </p>
+        <button class="btn btn-accent">
+          Get Started with
+          <Icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
