e2e522f7cfb1ba5a6ee004ac8f8272c05926a716
bret <bretai@LCARS-MX-23>
2025-06-24 11:32:33 +0300

    Setup tailwind && daisy-ui

--- app.vue
+++ app.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn btn-secondary">
+      <Icon name="uil:github" style="color: black; margin-right: 8px;" />
+      Secondary
+    </button>
   </div>
 </template>
