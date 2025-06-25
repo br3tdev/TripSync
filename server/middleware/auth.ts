755e7139969bd7367b60becba736bdff9483295d
bret <bretai@LCARS-MX-23>
2025-06-25 16:59:14 +0300

    :construction_worker:  Fix auth middleware for protected routes

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,11 +1,12 @@
 import { auth } from "~/lib/auth";
 
 export default defineEventHandler(async (event) => {
+  const session = await auth.api.getSession({
+    headers: event.headers,
+  });
+
   if (event.path.startsWith("/dashboard")) {
-    const session = auth.api.getSession({
-      headers: event.headers,
-    });
-    if (!session) {
+    if (!session?.user) {
       await sendRedirect(event, "/", 302);
     }
   }
