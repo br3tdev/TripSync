1800a00412f9c6a4324d5c38198e8d6653ea5e18
Bret Mda <146015182+br3tdev@users.noreply.github.com>
2025-06-25 17:30:50 +0300

    Tl 19 (#59)

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,12 +1,19 @@
 import { auth } from "~/lib/auth";
 
 export default defineEventHandler(async (event) => {
-  if (event.path.startsWith("/dashboard")) {
-    const session = auth.api.getSession({
+  const session = await auth.api.getSession({
       headers: event.headers,
     });
-    if (!session) {
-      await sendRedirect(event, "/", 302);
+
+    // eslint-disable-next-line no-console
+    console.log("Session:", session);
+
+    // eslint-disable-next-line no-console
+    console.log("Path:", event.path);
+
+    if (event.path.startsWith("/dashboard")) {
+      if (!session?.user) {
+        await sendRedirect(event, "/", 302);
+      }
     }
-  }
 });
