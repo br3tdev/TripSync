81c87c0e1226bbe958bea643e58d79cff360179d
bret <bretai@LCARS-MX-23>
2025-06-25 15:11:14 +0300

    :octocat: Add GitHub OAuth integration w/ Better auth

--- lib/env.ts
+++ lib/env.ts
@@ -7,6 +7,8 @@ const EnvSchema = z.object({
   TURSO_DATABASE_URL: z.string(),
   TURSO_AUTH_TOKEN: z.string(),
   BETTER_AUTH_SECRET: z.string(),
+  AUTH_GITHUB_CLIENT_ID: z.string(),
+  AUTH_GITHUB_CLIENT_SECRET: z.string(),
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
---
+++ pages/dashboard/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1 class="mb-7">
+      Welcome home
+    </h1>
+    <AuthButton />
+  </div>
+</template>
