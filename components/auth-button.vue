3884df547f547cb3998895712452cd8d4f6a6a8e
Bret Mda <146015182+br3tdev@users.noreply.github.com>
2025-06-25 16:31:53 +0300

    :arrows_counterclockwise::eye_speech_bubble: Add auth state and sign-out flow (#57)

--- components/auth-button.vue
+++ components/auth-button.vue
@@ -3,7 +3,26 @@ const authStore = useAuthStore();
 </script>
 
 <template>
+  <details v-if="!authStore.loading && authStore.user" class="dropdown dropdown-end">
+    <summary class="btn m-1">
+      <div v-if="authStore.user.image" class="avatar">
+        <div class="w-6 rounded-full">
+          <img :src="authStore.user.image" :alt="authStore.user.name">
+        </div>
+      </div>
+      {{ authStore.user.name }}
+    </summary>
+    <ul class="menu dropdown-content bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
+      <li>
+        <NuxtLink to="/sign-out">
+          <Icon name="tabler:logout-2" size="24" />
+          Sign Out
+        </NuxtLink>
+      </li>
+    </ul>
+  </details>
   <button
+    v-else
     :disabled="authStore.loading"
     class="btn btn-accent"
     @click="authStore.signIn"
