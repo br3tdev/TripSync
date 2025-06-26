7920777e5f3481469d41560b48674faab73dce21
bret <bretai@LCARS-MX-23>
2025-06-26 07:35:20 +0300

    :sidebar: Add collapsible sidebar component w/ :floppy_disk: Persist collapsed state in localStorage

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,8 +1,52 @@
+<script lang="ts" setup>
+const isSidebarOpen = ref(true);
+
+onMounted(() => {
+  isSidebarOpen.value = localStorage.getItem("isSidebarOpen") === "true";
+});
+
+function toggleSidebar() {
+  isSidebarOpen.value = !isSidebarOpen.value;
+  localStorage.setItem("isSidebarOpen", isSidebarOpen.value.toString());
+}
+</script>
+
 <template>
-  <div>
-    <h1 class="mb-7">
-      Welcome home
-    </h1>
-    <AuthButton />
+  <div class="flex-1 flex">
+    <div class="bg-base-100 transition-all duration-300 ease-in-out" :class="{ 'w-64': isSidebarOpen, 'w-16': !isSidebarOpen }">
+      <div
+        class="flex hover:cursor-pointer hover:bg-base-200"
+        :class="{ 'justify-end': isSidebarOpen, 'justify-center': !isSidebarOpen }"
+        @click="toggleSidebar"
+      >
+        <Icon
+          name="tabler:chevron-left"
+          :class="{ 'rotate-180 transition-all duration-300 ease-in-out': !isSidebarOpen }"
+          size="32"
+        />
+      </div>
+      <div class="flex flex-col">
+        <SidebarButton
+          label="Locations"
+          icon="tabler:map"
+          href="/dashboard"
+          :show-label="isSidebarOpen"
+        />
+        <SidebarButton
+          label="Add locations"
+          icon="tabler:circle-plus-filled"
+          href="/dashboard/add"
+          :show-label="isSidebarOpen"
+        />
+        <div class="divider" />
+        <SidebarButton
+          label="Sign Out"
+          icon="tabler:logout-2"
+          href="/sign-out"
+          :show-label="isSidebarOpen"
+        />
+      </div>
+    </div>
+    <div class=" flex-1 bg-amber-300" />
   </div>
 </template>
