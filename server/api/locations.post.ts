de049df7b6e540caffdeef531ffc1f372ebf8c91
bret <bretai@LCARS-MX-23>
2025-06-27 07:39:02 +0300

    :abc: Add slugify utility :globe_with_meridians: Handle location name collisions

--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -1,7 +1,12 @@
+import { and, eq } from "drizzle-orm";
+import { customAlphabet } from "nanoid";
+import slugify from "slug";
 import { z } from "zod";
 
 import db from "~/lib/db";
 import { location } from "~/lib/db/schema";
+
+const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxy", 5);
 
 const locationSchema = z.object({
   name: z.string().min(1).max(100),
@@ -43,11 +48,48 @@ export default defineEventHandler(async (event) => {
     }));
   }
 
-  const [created] = await db.insert(location).values({
-    ...result.data,
-    slug: result.data.name.replaceAll(" ", "-").toLocaleLowerCase(),
-    userId: event.context.user.id,
-  }).returning();
+  const existingLocation = await db.query.location.findFirst({
+    where:
+      and(
+        eq(location.name, result.data.name),
+        eq(location.userId, event.context.user.id),
+      ),
+  });
 
-  return created;
+  if (existingLocation) {
+    return sendError(event, createError({
+      statusCode: 409,
+      statusMessage: "A location with that name already exists!",
+    }));
+  }
+
+  let slug = slugify(result.data.name);
+  let slugExists = !!(await db.query.location.findFirst({
+    where: eq(location.slug, slug),
+  }));
+
+  while (slugExists) {
+    const id = nanoid();
+    const idSlug = `${slug}-${id}`;
+    slugExists = !!(await db.query.location.findFirst({
+      where: eq(location.slug, idSlug),
+    }));
+    if (!slugExists) {
+      slug = idSlug;
+    }
+  }
+
+  try {
+    const [created] = await db.insert(location).values({
+      ...result.data,
+      slug,
+      userId: event.context.user.id,
+    }).returning();
+
+    return created;
+  }
+  catch (error) {
+    console.error(error);
+    throw error;
+  }
 });
