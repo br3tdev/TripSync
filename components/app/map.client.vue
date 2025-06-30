53f71559f990819ea380abddde78b841f1448101
Bret Mda <146015182+br3tdev@users.noreply.github.com>
2025-06-30 08:40:29 +0300

    :world_map: Add Mapbox/Maplibre with theme support and display on dashboard (#68)

---
+++ components/app/map.client.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import { CENTER_UK } from "~/lib/constants";
+
+const colorMode = useColorMode();
+
+const style = computed(() => colorMode.value === "dark"
+  ? "/styles/dark.json"
+  : "https://tiles.openfreemap.org/styles/liberty");
+
+const zoom = 4;
+</script>
+
+<template>
+  <MglMap
+    :map-style="style"
+    :center="CENTER_UK"
+    :zoom="zoom"
+  >
+    <MglNavigationControl />
+  </MglMap>
+</template>
