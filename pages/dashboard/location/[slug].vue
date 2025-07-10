3374f083f0a016909f49460de6062c5c223ce665
bret <bretai@LCARS-MX-23>
2025-07-10 08:17:31 +0300

    :wastebasket: Add delete location API & UI

--- pages/dashboard/location/[slug].vue
+++ pages/dashboard/location/[slug].vue
@@ -1,5 +1,13 @@
 <script lang="ts" setup>
+import type { FetchError } from "ofetch";
+
+import getFetchErrorMessage from "~/util/get-fetch-error-message";
+
 const route = useRoute();
+
+const isOpen = ref(false);
+const isDeleting = ref(false);
+const deleteError = ref("");
 
 const locationStore = useLocationStore();
 const {
@@ -7,6 +15,32 @@ const {
   currentLocationError: error,
   currentLocationStatus: status,
 } = storeToRefs(locationStore);
+
+const loading = computed(() => status.value === "pending" || isDeleting.value);
+const errorMessage = computed(() => error.value?.statusMessage || deleteError.value);
+
+function openDialog() {
+  isOpen.value = true;
+  (document.activeElement as HTMLAnchorElement).blur();
+}
+
+async function onConfirmDelete() {
+  try {
+    isOpen.value = false;
+    deleteError.value = "";
+    isDeleting.value = true;
+    await $fetch(`/api/location/${route.params.slug}`, {
+      method: "DELETE",
+    });
+    navigateTo("/dashboard");
+  }
+  catch (e) {
+    const error = e as FetchError;
+    deleteError.value = getFetchErrorMessage(error);
+    console.error(deleteError.value);
+  }
+  isDeleting.value = false;
+}
 
 onMounted(() => {
   locationStore.refreshCurrentLocation();
@@ -21,11 +55,11 @@ onBeforeRouteUpdate((to) => {
 
 <template>
   <div class="p-4 min-h-64">
-    <div v-if="status === 'pending'">
+    <div v-if="loading">
       <div class="loading" />
     </div>
 
-    <div v-if="error">
+    <div v-if="errorMessage && !loading">
       <div class="alert alert-error">
         <Icon
           name="tabler:alert-hexagon"
@@ -33,14 +67,42 @@ onBeforeRouteUpdate((to) => {
           size="24"
         />
         <span>
-          {{ error.statusMessage }}
+          {{ errorMessage }}
         </span>
       </div>
     </div>
 
-    <div v-else-if="route.name === 'dashboard-location-slug' && location">
+    <div v-else-if="route.name === 'dashboard-location-slug' && location && !loading">
       <h3 class="text-xl">
         {{ location?.name }}
+
+        <div class="dropdown dropdown-bottom">
+          <div
+            tabindex="0"
+            role="button"
+            class="btn m-1 p-0"
+          >
+            <Icon name="tabler:dots-vertical" size="20" />
+          </div>
+          <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+            <li>
+              <NuxtLink
+                to=""
+                class="flex items-center"
+                @click="openDialog"
+              >
+                <Icon name="tabler:trash-x-filled" size="20" />
+                Delete
+              </NuxtLink>
+            </li>
+            <li>
+              <NuxtLink :to="{ name: 'dashboard-location-slug-edit', params: { slug: route.params.slug } }" class="flex items-center">
+                <Icon name="tabler:map-pin-cog" size="20" />
+                Edit
+              </NuxtLink>
+            </li>
+          </ul>
+        </div>
       </h3>
       <p class="text-sm">
         {{ location.description }}
@@ -57,5 +119,14 @@ onBeforeRouteUpdate((to) => {
     <div v-else-if="route.name !== 'dashboard-location-slug'">
       <NuxtPage />
     </div>
+    <AppDialog
+      title="Are you sure?"
+      description="Deleting this location will also delete all of the associated logs. This cannot be undone. Are you sure you want to do this?"
+      confirm-label="Yes, I am sure"
+      confirm-class="btn-error"
+      :is-open="isOpen"
+      @on-closed="isOpen = false"
+      @on-confirmed="onConfirmDelete"
+    />
   </div>
 </template>
