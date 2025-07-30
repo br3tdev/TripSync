27ae35596f56cfaba219256dd96dc7dfeb3d1a95
Bret Mda <146015182+br3tdev@users.noreply.github.com>
2025-07-30 11:27:57 +0300

    :trash: Add Delete locationLogImage API (#85)

--- components/image-list.vue
+++ components/image-list.vue
@@ -15,8 +15,9 @@ const config = useRuntimeConfig();
       :key="image.id"
       class="card card-compact h-40 w-64 shrink-0 flex items-center justify-center bg-base-300"
     >
-      <div class="card-body size-full">
+      <div class="relative card-body size-full">
         <img class="size-full object-cover rounded-xs" :src="`${config.public.s3BucketUrl}/${image.key}`">
+        <slot :image />
       </div>
     </div>
   </div>
